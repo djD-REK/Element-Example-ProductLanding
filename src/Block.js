17fb2d50877d2445a5fb25be594ae010262862bb
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-05-06 19:32:50 -0400

    Version 1.1

--- src/Block.js
+++ src/Block.js
@@ -4,22 +4,24 @@ import { getStyles } from "./getStyles"
 import { defaultConfig } from "./configs"
 
 const Block = (props) => {
-  const classes = StyleSheet.create(getStyles(props))
-
   const product = props.data
   if (!product.id) {
-    return (
-      <div>
-        <h1 className={css(classes.example)}>{props.text}</h1>
-        <h1>Product Landing Block</h1>
-      </div>
-    )
+    return <h1>Product Landing Block</h1>
+  }
+  const addToCart = () => {
+    const productVariant = {
+      productId: product.id,
+      quantity: 1,
+      variantId: product.productVariants[0].id,
+      itemPrice: product.productVariants[0].price,
+    }
+    props.pubSub.publish(props.events.cart.addToCart, productVariant)
   }
   return (
-    <div>
-      <h1 className={css(classes.example)}>{props.text}</h1>
+    <React.Fragment>
       <h1>{product.name}</h1>
-    </div>
+      <button onClick={addToCart}>Add to Cart</button>
+    </React.Fragment>
   )
 }
 
